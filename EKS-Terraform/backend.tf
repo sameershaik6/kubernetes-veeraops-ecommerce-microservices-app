ceafe4b18d5d2d31a6eddf72386685fce1d4408b
sameershaik <99790327+sameershaik6@users.noreply.github.com>
2026-09-20 12:17:41 +0530

    Update S3 bucket name in backend configuration

--- EKS-Terraform/backend.tf
+++ EKS-Terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "k8demowithveeraecommerce"
+    bucket       = "k8sammdemoprojectbuk"
     key          = "terraform.tfstate"
     region       = "us-east-1"
     use_lockfile = true
